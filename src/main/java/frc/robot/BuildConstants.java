8d2cd4b9fc378ad59dbc58bdbf30b336149b4678
Saathvik k <saathvikkamma@gmail.com>
2026-01-27 21:21:36 -0600

    removed unnesseasry print

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 21;
-  public static final String GIT_SHA = "e577b5c70f7be54d844660ef478ab95870916f86";
-  public static final String GIT_DATE = "2026-01-26 14:35:15 CST";
+  public static final int GIT_REVISION = 22;
+  public static final String GIT_SHA = "fdd891c7c6edcddbce9fe5d68f9baccc43272068";
+  public static final String GIT_DATE = "2026-01-27 20:37:38 CST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-27 20:36:55 CST";
-  public static final long BUILD_UNIX_TIME = 1769567815284L;
+  public static final String BUILD_DATE = "2026-01-27 21:19:00 CST";
+  public static final long BUILD_UNIX_TIME = 1769570340293L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
