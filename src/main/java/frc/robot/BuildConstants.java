679bcf17f624a91414799b8880f15569ec51189a
Saathvik k <saathvikkamma@gmail.com>
2026-02-01 11:56:11 -0600

    starting to work on the double shooter

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 30;
-  public static final String GIT_SHA = "8ebd58f974d0c50f9921b005b8dfea7469ca2226";
-  public static final String GIT_DATE = "2026-01-31 00:42:19 CST";
+  public static final int GIT_REVISION = 31;
+  public static final String GIT_SHA = "92f759f01becef4dd8badd19a82f3d720c5d9bd3";
+  public static final String GIT_DATE = "2026-01-31 23:41:21 CST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-31 23:39:22 CST";
-  public static final long BUILD_UNIX_TIME = 1769924362162L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-01 11:54:14 CST";
+  public static final long BUILD_UNIX_TIME = 1769968454716L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
