9f1e5c2fb9803ec503c75b74337c62d0e5c66f8a
AaronZhenger <alock8mini@gmail.com>
2026-02-01 20:09:42 -0600

    Current 3 motors

--- src/main/java/team5427/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/team5427/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -1,3 +1,54 @@
 package team5427.frc.robot.subsystems.shooter;
 
-public final class ShooterConstants {}
+import team5427.lib.drivers.CANDeviceId;
+import team5427.lib.drivers.ComplexGearRatio;
+import team5427.lib.motors.MotorConfiguration;
+import team5427.lib.motors.MotorConfiguration.IdleState;
+import team5427.lib.motors.MotorConfiguration.MotorMode;
+import team5427.lib.motors.MotorUtil;
+
+public final class ShooterConstants {
+    public static final CANDeviceId kLeftHoodMotorCanId = new CANDeviceId(30);
+    public static final CANDeviceId kLeftFlywheelLeaderMotorCanId = new CANDeviceId(31);
+    public static final CANDeviceId kLeftFlywheelFollowerMotorCanId = new CANDeviceId(32);
+    
+    public static final CANDeviceId kRightHoodMotorCanId = new CANDeviceId(33);
+    public static final CANDeviceId kRightFlywheelLeaderMotorCanId = new CANDeviceId(34);
+    public static final CANDeviceId kRightFlywheelFollowerMotorCanId = new CANDeviceId(35);
+    
+    public static final MotorConfiguration kHoodMotorConfiguration = new MotorConfiguration();
+    public static final MotorConfiguration kFlywheelConfiguration = new MotorConfiguration();
+
+    public static final ComplexGearRatio kHoodMotorGearRatio = new ComplexGearRatio((30.0/12.0));
+    public static final ComplexGearRatio kFlywheelLeaderGearRatio = new ComplexGearRatio((36.0/30.0));
+    public static final ComplexGearRatio kFlywheelFollowerGearRatio = new ComplexGearRatio((18.0/14.0));
+
+    static {
+        kHoodMotorConfiguration.gearRatio = kHoodMotorGearRatio;
+        kHoodMotorConfiguration.isArm = false;
+        kHoodMotorConfiguration.idleState = IdleState.kBrake;
+        kHoodMotorConfiguration.isInverted = false;
+        kHoodMotorConfiguration.mode = MotorMode.kLinear;
+        kHoodMotorConfiguration.withFOC = true;
+
+        kHoodMotorConfiguration.maxVelocity =
+            kHoodMotorConfiguration.getStandardMaxVelocity(MotorUtil.kKrakenX60FOC_MaxRPM);
+        kHoodMotorConfiguration.maxAcceleration = kHoodMotorConfiguration.maxVelocity * 3.0;
+
+        kHoodMotorConfiguration.altV = kHoodMotorConfiguration.maxVelocity / 2.0;
+        kHoodMotorConfiguration.altA = kHoodMotorConfiguration.maxAcceleration;
+        kHoodMotorConfiguration.altJ = 1000.0;
+
+        kHoodMotorConfiguration.kP = 1.0;
+        kHoodMotorConfiguration.kI = 0.0;
+        kHoodMotorConfiguration.kD = 0.0;
+
+        kHoodMotorConfiguration.kV = 0.0;
+        kHoodMotorConfiguration.kA = 0.0;
+        kHoodMotorConfiguration.kS = 0.0;
+        kHoodMotorConfiguration.kG = 1.0;
+        kHoodMotorConfiguration.kFF = 0.0;
+
+        kHoodMotorConfiguration.currentLimit = 40;
+    }
+}
--- src/main/java/team5427/frc/robot/subsystems/shooter/io/ShooterIOTalonFX.java
+++ src/main/java/team5427/frc/robot/subsystems/shooter/io/ShooterIOTalonFX.java
@@ -1,5 +1,10 @@
 package team5427.frc.robot.subsystems.shooter.io;
 
+import com.ctre.phoenix6.StatusSignal;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearVelocity;
+import team5427.frc.robot.subsystems.shooter.ShooterConstants;
 import team5427.lib.motors.SteelTalonFX;
 
 public class ShooterIOTalonFX implements ShooterIO {
@@ -14,7 +19,35 @@ public class ShooterIOTalonFX implements ShooterIO {
   private SteelTalonFX rightFlywheelLeaderMotor;
   private SteelTalonFX rightFlywheelFollowerMotor;
 
-  public ShooterIOTalonFX() {}
+  private StatusSignal<Boolean> leftHoodMotorConnected;
+  private StatusSignal<Boolean> leftFlywheelMotorLeaderConnected;
+  private StatusSignal<Boolean> leftFlywheelMotorFollowerConnected;
+
+  private StatusSignal<Double> leftHoodMotorPositionRadians;
+  private StatusSignal<Double> leftHoodMotorAngularVelocityRadiansPerSecond;
+  private StatusSignal<AngularVelocity> leftFlywheelMotorAngularSpeed;
+  private StatusSignal<LinearVelocity> leftFlywheelMotoLinearSpeed;
+
+  private StatusSignal<Boolean> rightHoodMotorConnected;
+  private StatusSignal<Boolean> rightFlywheelMotorLeaderConnected;
+  private StatusSignal<Boolean> rightFlywheelMotorFollowerConnected;
+
+  private StatusSignal<Double> rightHoodMotorPositionRadians;
+  private StatusSignal<Double> rightHoodMotorAngularVelocityRadiansPerSecond;
+  private StatusSignal<AngularVelocity> rightFlywheelMotorAngularSpeed;
+  private StatusSignal<LinearVelocity> rightFlywheelMotoLinearSpeed;
+
+  public ShooterIOTalonFX() {
+    leftHoodMotor = new SteelTalonFX(ShooterConstants.kLeftHoodMotorCanId);
+    leftFlywheelLeaderMotor = new SteelTalonFX(ShooterConstants.kLeftFlywheelLeaderMotorCanId);
+    leftFlywheelFollowerMotor = new SteelTalonFX(ShooterConstants.kLeftFlywheelFollowerMotorCanId);
+
+    rightHoodMotor = new SteelTalonFX(ShooterConstants.kRightHoodMotorCanId);
+    rightFlywheelLeaderMotor = new SteelTalonFX(ShooterConstants.kRightFlywheelLeaderMotorCanId);
+    rightFlywheelFollowerMotor = new SteelTalonFX(ShooterConstants.kRightFlywheelFollowerMotorCanId);
+
+
+  }
 
   @Override
   public void updateInputs(ShooterIOInputs inputs) {
