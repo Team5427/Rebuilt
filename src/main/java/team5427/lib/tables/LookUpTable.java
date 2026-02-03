3c5e7f1cc63de9d7a01f51647fcbd94c42c73c6f
Saathvik Kamma <saathvikkamma@gmail.com>
2026-02-03 19:54:41 +0000

    updated look up table names

--- src/main/java/team5427/lib/tables/LookUpTable.java
+++ src/main/java/team5427/lib/tables/LookUpTable.java
@@ -2,21 +2,17 @@ package team5427.lib.tables;
 
 import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
 
-// Distance -> Pivot Angle
-// Distance -> Flywheel 1 speed
-// Distance -> Flywheel 2 speed
-
 public class LookUpTable {
   private InterpolatingTreeMap<Double, Double> pivotAngleLookup;
-  private InterpolatingTreeMap<Double, Double> flywheel1Lookup;
-  private InterpolatingTreeMap<Double, Double> flywheel2Lookup;
+  private InterpolatingTreeMap<Double, Double> flywheelLookup;
+  private InterpolatingTreeMap<Double, Double> timeOfFlightLookup;
 
   public LookUpTable() {
     pivotAngleLookup =
         new InterpolatingTreeMap<>(new TableInverseInterpolator(), new TableInterpolator());
-    flywheel1Lookup =
+    flywheelLookup =
         new InterpolatingTreeMap<>(new TableInverseInterpolator(), new TableInterpolator());
-    flywheel2Lookup =
+    timeOfFlightLookup =
         new InterpolatingTreeMap<>(new TableInverseInterpolator(), new TableInterpolator());
   }
 
@@ -24,23 +20,23 @@ public class LookUpTable {
     pivotAngleLookup.put(distance, pivotAngle);
   }
 
-  public void addflyWheel1Speed(Double distance, Double speed) {
-    flywheel1Lookup.put(distance, speed);
+  public void addflyWheelSpeed(Double distance, Double speed) {
+    flywheelLookup.put(distance, speed);
   }
 
-  public void addflyWheel2Speed(Double distance, Double speed) {
-    flywheel2Lookup.put(distance, speed);
+  public void addtimeOfFlight(Double distance, Double time) {
+    timeOfFlightLookup.put(distance, time);
   }
 
   public Double getpivotAngle(Double distance) {
     return pivotAngleLookup.get(distance);
   }
 
-  public Double getflyWheel1Speed(Double distance) {
-    return flywheel1Lookup.get(distance);
+  public Double getflyWheelSpeed(Double distance) {
+    return flywheelLookup.get(distance);
   }
 
-  public Double getflyWheel2Speed(Double distance) {
-    return flywheel2Lookup.get(distance);
+  public Double gettimeOfFlight(Double distance) {
+    return timeOfFlightLookup.get(distance);
   }
 }
