c94bbc36fa1d82507232bab173777e4d461b1c7d
Saathvik k <saathvikkamma@gmail.com>
2026-02-09 20:11:42 -0600

    adjusted vision stds

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 43;
-  public static final String GIT_SHA = "9fdd00763b13c22332a12d2746e8726c97bc61cb";
-  public static final String GIT_DATE = "2026-02-07 23:10:51 CST";
+  public static final int GIT_REVISION = 45;
+  public static final String GIT_SHA = "61e627f00b5fe159f820f088a4b086f9d9979bc4";
+  public static final String GIT_DATE = "2026-02-09 19:32:07 CST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-08 01:08:09 CST";
-  public static final long BUILD_UNIX_TIME = 1770534489390L;
+  public static final String BUILD_DATE = "2026-02-09 19:48:48 CST";
+  public static final long BUILD_UNIX_TIME = 1770688128727L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
