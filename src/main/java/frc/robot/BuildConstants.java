d63ac7bd55f6024636657e97afc82ee05bd00d6f
Saathvik k <saathvikkamma@gmail.com>
2026-02-12 21:10:10 -0600

    minor

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,24 +1,17 @@
 package frc.robot;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 64;
-  public static final String GIT_SHA = "e78eef8b1e63b4e87f1a99697996beee56709578";
-  public static final String GIT_DATE = "2026-02-12 19:26:12 CST";
+  public static final int GIT_REVISION = 67;
+  public static final String GIT_SHA = "36a4aa10c37c90f6651329e144b1564203f98047";
+  public static final String GIT_DATE = "2026-02-12 21:09:28 CST";
   public static final String GIT_BRANCH = "main";
-<<<<<<< HEAD
-  public static final String BUILD_DATE = "2026-02-12 21:00:46 CST";
-  public static final long BUILD_UNIX_TIME = 1770951646365L;
-=======
-  public static final String BUILD_DATE = "2026-02-12 19:59:12 CST";
-  public static final long BUILD_UNIX_TIME = 1770947952791L;
->>>>>>> dfbc26964ac56ec930f2c21a7c1551b62f7414ff
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-12 21:09:41 CST";
+  public static final long BUILD_UNIX_TIME = 1770952181882L;
+  public static final int DIRTY = 0;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
