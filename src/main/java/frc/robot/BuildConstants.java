fe28bf1d8a0b527ab0dbee58025b3c3030a05118
Saathvik k <saathvikkamma@gmail.com>
2026-02-28 17:40:33 -0600

    updated advantage kit depedndecy

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 89;
-  public static final String GIT_SHA = "cb06c2c80f8e369b6e7285c4670605f7dea8d656";
-  public static final String GIT_DATE = "2026-02-28 13:03:35 CST";
+  public static final int GIT_REVISION = 90;
+  public static final String GIT_SHA = "fdbec1aff11ce5ee730acaee477b7894893365fc";
+  public static final String GIT_DATE = "2026-02-28 17:32:29 CST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-28 15:48:13 CST";
-  public static final long BUILD_UNIX_TIME = 1772315293354L;
+  public static final String BUILD_DATE = "2026-02-28 17:39:57 CST";
+  public static final long BUILD_UNIX_TIME = 1772321997998L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
