07403122e35856026407fdb8ea12a73c5da569a4
AaronZhenger <alock8mini@gmail.com>
2026-03-11 23:57:57 -0500

    Build Constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 133;
-  public static final String GIT_SHA = "fb54fc11658effd066a0e821d291e61c101d53e5";
-  public static final String GIT_DATE = "2026-03-11 15:54:21 CDT";
+  public static final int GIT_REVISION = 140;
+  public static final String GIT_SHA = "9826d4e8129b2e943b52a84202b1280b102c0477";
+  public static final String GIT_DATE = "2026-03-11 23:44:59 CDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-11 16:03:13 CDT";
-  public static final long BUILD_UNIX_TIME = 1773262993017L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-11 23:51:26 CDT";
+  public static final long BUILD_UNIX_TIME = 1773291086281L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
