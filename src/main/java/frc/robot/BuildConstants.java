b851bd043c75a40281feadc9bf483a967082e746
Saathvik k <saathvikkamma@gmail.com>
2026-03-16 23:25:20 -0500

    updated photonlib

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 161;
-  public static final String GIT_SHA = "377058ff6c792d6f58b3bfe5268daf246169ded4";
-  public static final String GIT_DATE = "2026-03-16 18:34:10 CDT";
+  public static final int GIT_REVISION = 162;
+  public static final String GIT_SHA = "239f134d114980b13446f23922341d1802552854";
+  public static final String GIT_DATE = "2026-03-16 23:19:35 CDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-16 23:15:21 CDT";
-  public static final long BUILD_UNIX_TIME = 1773720921917L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-03-16 23:23:49 CDT";
+  public static final long BUILD_UNIX_TIME = 1773721429856L;
+  public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
