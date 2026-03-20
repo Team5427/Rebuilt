a3453ad6b8c14a29a891b533383e058db7de0e5a
Saathvik k <saathvikkamma@gmail.com>
2026-03-19 19:30:19 -0500

    fixing some bugs and spotless apply

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 162;
-  public static final String GIT_SHA = "239f134d114980b13446f23922341d1802552854";
-  public static final String GIT_DATE = "2026-03-16 23:19:35 CDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-16 23:23:49 CDT";
-  public static final long BUILD_UNIX_TIME = 1773721429856L;
+  public static final int GIT_REVISION = 164;
+  public static final String GIT_SHA = "4aed6115d85857c0e40a7f18a27cae983b5a4fe5";
+  public static final String GIT_DATE = "2026-03-19 10:08:29 CDT";
+  public static final String GIT_BRANCH = "SOTMLibTest";
+  public static final String BUILD_DATE = "2026-03-19 19:29:20 CDT";
+  public static final long BUILD_UNIX_TIME = 1773966560306L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
