d0b8024717d9316b6919072261c44ed38f65b347
Saathvik k <saathvikkamma@gmail.com>
2026-04-06 20:58:38 -0500

    Update dependencies for ChoreoLib and photonlib

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 192;
-  public static final String GIT_SHA = "8ffddcd7a22117845df38209429b7e695b3365ba";
-  public static final String GIT_DATE = "2026-04-03 23:14:43 CDT";
+  public static final int GIT_REVISION = 196;
+  public static final String GIT_SHA = "05ce7e26e31e4f1056a99c3065605f8b726a943a";
+  public static final String GIT_DATE = "2026-04-06 19:29:32 CDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-04-04 13:39:10 CDT";
-  public static final long BUILD_UNIX_TIME = 1775327950151L;
+  public static final String BUILD_DATE = "2026-04-06 20:56:06 CDT";
+  public static final long BUILD_UNIX_TIME = 1775526966556L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
