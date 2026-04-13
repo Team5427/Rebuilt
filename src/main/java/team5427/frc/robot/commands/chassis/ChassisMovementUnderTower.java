2a907b1cd2f250be31efc9b09d3cd89ab66576c3
k1410586 <saathvikkamma@gmail.com>
2026-04-13 14:34:45 -0500

    calm changes making a new command to drive under the tower

---
+++ src/main/java/team5427/frc/robot/commands/chassis/ChassisMovementUnderTower.java
@@ -0,0 +1,81 @@
+package team5427.frc.robot.commands.chassis;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.RobotState;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import org.team4206.battleaid.common.TunedJoystick;
+import org.team4206.battleaid.common.TunedJoystick.ResponseCurve;
+import team5427.frc.robot.Constants.DriverConstants;
+import team5427.frc.robot.subsystems.Swerve.DrivingConstants;
+import team5427.frc.robot.subsystems.Swerve.SwerveSubsystem;
+
+public class ChassisMovementUnderTower extends Command {
+
+  private SwerveSubsystem swerveSubsystem;
+  private CommandXboxController joy;
+
+  private TunedJoystick translationJoystick;
+  private TunedJoystick rotationJoystick;
+
+  private boolean isRed;
+
+  public ChassisMovementUnderTower(CommandXboxController driverJoystick) {
+    swerveSubsystem = SwerveSubsystem.getInstance();
+    joy = driverJoystick;
+    translationJoystick = new TunedJoystick(joy.getHID());
+    translationJoystick.useResponseCurve(ResponseCurve.LINEAR);
+
+    rotationJoystick = new TunedJoystick(joy.getHID());
+    rotationJoystick.useResponseCurve(ResponseCurve.QUADRATIC);
+
+    translationJoystick.setDeadzone(DriverConstants.kDriverControllerJoystickDeadzone);
+    rotationJoystick.setDeadzone(DriverConstants.kDriverControllerJoystickDeadzone);
+    addRequirements(swerveSubsystem);
+  }
+
+  @Override
+  public void execute() {
+    if (DriverStation.isTeleop()) {
+      isRed =
+          DriverStation.getAlliance().isPresent()
+              && DriverStation.getAlliance().get() == Alliance.Red;
+      double vx = -translationJoystick.getLeftY();
+      double vy = -translationJoystick.getLeftX();
+        Rotation2d nearestLockedRotation =
+              RobotState.getInstance().getRotation().getRadians() > 0
+                  ? Rotation2d.kCCW_90deg
+                  : Rotation2d.kCW_90deg;
+     double omegaRadians = DrivingConstants.kRotationController.calculate(nearestLockedRotation.minus(RobotState.getInstance().getRotation()),0); 
+      if (isRed) {
+        vx *= -1;
+        vy *= -1;
+      }
+
+      double dampener = (joy.getRightTriggerAxis() * DrivingConstants.kDampenerDampeningAmount);
+
+      ChassisSpeeds driverSpeeds =
+          swerveSubsystem.getDriveSpeeds(vx, vy, omegaRadians, dampener).times(0.75);
+
+      if (joy.getLeftTriggerAxis() >= 0.1) {
+        driverSpeeds = new ChassisSpeeds(0, 0, 0);
+      }
+      swerveSubsystem.setInputSpeeds(driverSpeeds);
+    } else {
+      swerveSubsystem.setInputSpeeds(new ChassisSpeeds(0, 0, 0));
+    }
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    swerveSubsystem.setInputSpeeds(new ChassisSpeeds());
+  }
+}
